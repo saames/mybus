a2f31f94cd62bf376801ab5a4aa3e7e92bd9acf9
saames <samuel.calderadias@gmail.com>
2025-09-17 20:14:45 -0500

    feat: Adiciona restrições para tornar o botão Acessar clicável

--- mybusapp/view/login_view.py
+++ mybusapp/view/login_view.py
@@ -8,29 +8,49 @@ class LoginView:
         self.janela.geometry('640x480')
         self.frm_center = ttk.Frame(self.janela)
         self.frm_center.pack()
-
         
-        self.img_logo = ttk.PhotoImage(file='../mybusapp/resources/media/mybus_logo_128.png')
-        #self.img_logo = self.img_logo.subsample(1,1)
+        # Logo MyBus
+        self.img_logo = ttk.PhotoImage(file='./mybusapp/resources/media/mybus_logo_128.png')
         self.lbl_logo = ttk.Label(self.frm_center, image=self.img_logo)
         self.lbl_logo.image = self.img_logo
         self.lbl_logo.grid(column=0, row=0, columnspan=2, pady=20)
 
+        # Nome de Usuário (CPF)
         self.lbl_username = ttk.Label(self.frm_center, text='CPF', bootstyle='inverse-secondary', 
                                                                    borderwidth=7, 
                                                                    padding=(13,0),
                                                                    font=('TkDefaultFont', 10, 'bold'))
         self.lbl_username.grid(column=0, row=1)
-        self.ent_username = ttk.Entry(self.frm_center, validate='key', validatecommand=LoginControl.validar_campos)
+        self.ent_username = ttk.Entry(self.frm_center)
         self.ent_username.grid(column=1, row=1)
+        self.ent_username.bind('<KeyRelease>', self.validar_campos)
 
+        # Senha
         self.lbl_password = ttk.Label(self.frm_center, text='SENHA', bootstyle='inverse-secondary', 
                                                                      borderwidth=7, 
                                                                      padding=(2,0),
                                                                      font=('TkDefaultFont', 10, 'bold'))
         self.lbl_password.grid(column=0, row=2)
-        self.ent_password = ttk.Entry(self.frm_center, show='*', validate='key', validatecommand=LoginControl.validar_campos)
+        self.ent_password = ttk.Entry(self.frm_center, show='*')
         self.ent_password.grid(column=1, row=2, pady=5)
+        self.ent_password.bind('<KeyRelease>', self.validar_campos)
 
+        # Botão Logar
         self.btn_acessar = ttk.Button(self.frm_center, text='ACESSAR', state='disabled')
         self.btn_acessar.grid(column=0, row=3, columnspan=2, sticky='we', pady=5)
+        self.btn_acessar.bind('<ButtonRelease-1>', LoginControl.autenticar)
+
+        # Botão Cadastrar
+        self.btn_cadastrar = ttk.Button(self.frm_center, text='Não possuo cadastro', bootstyle='LINK')
+        self.btn_cadastrar.grid(column=0, row=4, columnspan=2, pady=10)
+        self.btn_acessar.bind('<ButtonRelease-1>')
+
+    
+    # Restrições básicas para autenticação
+    def validar_campos(self, event):
+        cpf = self.ent_username.get()
+        senha = self.ent_password.get()
+        if len(cpf)==11 and len(senha) >= 8: # CPF tem tamanho 11 e senha maior ou igual 8.
+            self.btn_acessar.config(state='enable')
+        else:
+            self.btn_acessar.config(state='disabled')
