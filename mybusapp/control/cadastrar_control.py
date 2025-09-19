4095642afcd59c3124a968f65ad2d647fd00eae9
Vitor5005 <joao.vitor.2004.f.s@gmail.com>
2025-09-19 13:06:37 -0500

    feat: Adicionando o cadatrar_control para poder realizar o cadastro de usuarios.

---
+++ mybusapp/control/cadastrar_control.py
@@ -0,0 +1,12 @@
+
+from mybusapp.model.usuario_model import UsuarioModel
+
+class Cadastra_control:
+
+    def Cadastrar_usuario(self, nome, cpf, telefone, senha, papel, status):
+        user_model = UsuarioModel()
+        result = user_model.insert("Usuario(nome, cpf, telefone, senha, papel, status)", (nome, cpf, telefone, senha, papel, status))
+        if(result):
+            return result
+        else:
+            return None
