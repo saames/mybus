29b0dbaf3a8caf38c8918012353509045ab2f9ed
Vitor5005 <joao.vitor.2004.f.s@gmail.com>
2025-09-19 09:52:27 -0500

    feat: Criando o model de usuario para que assim quando o o metodo delete seja chamado para a tabela usuario, o usuario não seja deletado mas que apenas o seu status mude de ativo para inativo.

---
+++ mybusapp/model/usuario_model.py
@@ -0,0 +1,20 @@
+from mybusapp.model.model import Model
+from sqlite3 import Error
+
+class UsuarioModel(Model):
+
+    def __int__(self):
+        Model()
+
+    def delete(self, table, id):
+        sql = f"UPDATE Usuario SET status = 'I' where id = {id};"
+        try:
+            con = self.conexao.get_conexao()
+            cursor = con.cursor()
+            result = cursor.execute(sql).rowcount
+            con.commit()
+            con.close()
+            return result
+        except Error as er:
+            print(er)
+
