128c8cae3148c430b5d8a871b1e79918b2fa9109
kelvin.moreira <kelvinmreira2022@gmail.com>
2025-09-21 16:49:56 -0500

    feat: adiciona (parcialmente) a janela gerenciar onibus

---
+++ mybusapp/control/gerenciar_onibus_control.py
@@ -0,0 +1,8 @@
+class GerenciarOnibusControl:
+    def listar_onibus(self, nome=''):
+        return (('1','10000' ,'ABC-1234','1', 'Urbano'),
+               ('2', '20000','DEF-5678', '0', 'None'),
+               ('3', '30000','GHI-9012', '1', 'Rodoviário'),
+               ('4', '40000','JKL-3456', '0', 'None'),
+               ('5', '50000','MNO-7890', '1','Metropolitano'),
+               )
