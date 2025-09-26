e1982932e8b875a85cfbf220685eedfdf5595294
Vitor5005 <joao.vitor.2004.f.s@gmail.com>
2025-09-26 15:21:11 -0500

    feat: Menssagem caso não haja um rota relacionada com a linha.

--- mybusapp/view/user_view/visualizar_rota/visualizar_rota_view.py
+++ mybusapp/view/user_view/visualizar_rota/visualizar_rota_view.py
@@ -33,38 +33,39 @@ class VisualizarRotaView:
         self.lbl_nome_rota = ttk.Label(self.frm_center, text=self.nome_rota, bootstyle='primary-inverse', padding=(150, 11))
         self.lbl_nome_rota.grid(column=1, row=0)
 
-        # Mapa
-        self.mpv_rota = tkmap.TkinterMapView(self.frm_center, width=554, height=350) # Cria mapa
-        self.mpv_rota.set_position(-9.972802894375437, -67.82629104665347) # Define a posição em Rio Branco
-        self.mpv_rota.set_zoom(13) # Ajusta o zoom
         self.traçar_rota()
-        self.mpv_rota.grid(column=0, row=1, columnspan=2)
-
-        # Legenda
-        self.lbl_legenda = ttk.Label(self.frm_center, text=' Legenda: ', bootstyle='primary-inverse')
-        self.lbl_legenda.grid(column=0, row=2, columnspan=2 , sticky='we', pady=8)
-
-
-        self.nome_completo = self.nome_completo.split("–")
-        self.nome_origem = self.nome_completo[0].strip()
-        self.nome_destino = self.nome_completo[1].strip()
-
-        self.lbl_ida_cor = ttk.Label(self.frm_center, bootstyle='info-inverse')
-        self.lbl_ida_cor.grid(column=0, row=3, ipadx=8, ipady=0)
-        self.lbl_ida_nome = ttk.Label(self.frm_center, text=f"{self.nome_origem} / {self.nome_destino}")
-        self.lbl_ida_nome.grid(column=1, row=3, sticky='w')
-
-        self.lbl_volta_cor = ttk.Label(self.frm_center, bootstyle='danger-inverse')
-        self.lbl_volta_cor.grid(column=0, row=4, ipadx=8, ipady=0, pady=5)
-        self.lbl_volta_nome = ttk.Label(self.frm_center, text=f"{self.nome_destino} / {self.nome_origem}")
-        self.lbl_volta_nome.grid(column=1, row=4, sticky='w')
 
         self.utils.centraliza(self.janela)
 
     def traçar_rota(self):
         result = self.rota_control.buscar_rotas_da_linha(self.linha_id)
         if(len(result) > 1):
+            # Mapa
+            self.mpv_rota = tkmap.TkinterMapView(self.frm_center, width=554, height=350) # Cria mapa
+            self.mpv_rota.set_position(-9.972802894375437, -67.82629104665347) # Define a posição em Rio Branco
+            self.mpv_rota.set_zoom(13) # Ajusta o zoom
             self.mpv_rota.set_path(result[0], color="#0B67CD", width=5)
             self.mpv_rota.set_path(result[1], color="red", width=4)
+            self.mpv_rota.grid(column=0, row=1, columnspan=2)
+
+            # Legenda
+            self.lbl_legenda = ttk.Label(self.frm_center, text=' Legenda: ', bootstyle='primary-inverse')
+            self.lbl_legenda.grid(column=0, row=2, columnspan=2 , sticky='we', pady=8)
+
+
+            self.nome_completo = self.nome_completo.split("–")
+            self.nome_origem = self.nome_completo[0].strip()
+            self.nome_destino = self.nome_completo[1].strip()
+
+            self.lbl_ida_cor = ttk.Label(self.frm_center, bootstyle='info-inverse')
+            self.lbl_ida_cor.grid(column=0, row=3, ipadx=8, ipady=0)
+            self.lbl_ida_nome = ttk.Label(self.frm_center, text=f"{self.nome_origem} / {self.nome_destino}")
+            self.lbl_ida_nome.grid(column=1, row=3, sticky='w')
+
+            self.lbl_volta_cor = ttk.Label(self.frm_center, bootstyle='danger-inverse')
+            self.lbl_volta_cor.grid(column=0, row=4, ipadx=8, ipady=0, pady=5)
+            self.lbl_volta_nome = ttk.Label(self.frm_center, text=f"{self.nome_destino} / {self.nome_origem}")
+            self.lbl_volta_nome.grid(column=1, row=4, sticky='w')
+            
         else:
             messagebox.showerror("Error", "Essa linha não possui rota cadastrada.")
