0a37c31864a5ae0e2c10673d9e08f14a31585a0c
saames <samuel.calderadias@gmail.com>
2025-10-03 11:33:23 -0500

    fix: Adiciona email

--- mybusapp/control/cadastrar_control.py
+++ mybusapp/control/cadastrar_control.py
@@ -6,14 +6,14 @@ class Cadastra_control:
     def __init__(self):
         self.user_model = UsuarioModel()
 
-    def Cadastrar_usuario(self, nome, cpf, telefone, senha, papel, status):
-        result = self.user_model.insert("Usuario(nome, cpf, telefone, senha, papel, status)", (nome, cpf, telefone, senha, papel, status))
+    def Cadastrar_usuario(self, nome, cpf, telefone, senha, papel, status, email):
+        result = self.user_model.insert("Usuario(nome, cpf, telefone, senha, papel, status, email)", (nome, cpf, telefone, senha, papel, status, email))
         if(result):
             return result
         else:
             return None
 
-    def editar_usuario(self, id, nome, cpf, senha, telefone, papel, status):
-        result = self.user_model.update("Usuario(nome, cpf, telefone, papel, status, senha)",
-                                   (f"'{nome}'", f"'{cpf}'", f"'{telefone}'", f"'{papel}'", f"'{status}'", f"'{senha}'"), id)
+    def editar_usuario(self, id, nome, cpf, senha, telefone, papel, status, email):
+        result = self.user_model.update("Usuario(nome, cpf, telefone, papel, status, senha, email)",
+                                   (f"'{nome}'", f"'{cpf}'", f"'{telefone}'", f"'{papel}'", f"'{status}'", f"'{senha}'", f"'{email}'"), id)
         return result
